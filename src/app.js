b09a1b2f38e3bad6e37803275607c6ff7ea4e5e7
yooseungyeop <yooseungyub>
2024-05-25 19:10:24 +0900

    jwt구성AccessToken반환

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { errorHandler } from './middlewares/error-handler.middleware.js';
 import UserRouter from './routers/auth.router.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3306;
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import { prisma } from '../utils/prisma.util.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
+
+// 액세스 토큰 생성 함수
+// AccessToken(Payload에 사용자 ID를 포함하고 유효기간이 12시간)을 생성합니다.
+const generateAccessToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '12h' });
+};
+
 // 회원가입 API 
 router.post('/sign-up', async (req, res, next) => {
     // 이메일 비밀번호 비밀번호 확인 이름을 req.body로 전달 받습니다.
@@ -86,22 +94,19 @@ router.post('/sign-in', async (req, res, next) => {
     }
     // → 이메일로 조회가 불가하거나 비밀번호가 다를 경우 - “인증 정보가 유효하지 않습니다”
     const user = await prisma.user.findUnique({
-        where: {
-            email: email
-        }
+        where: { email: email }        // DB안의 email 컬럼안의 값이 req.body로 전달된email과 일치하는걸 user로 선언
     });
 
-    if (!user) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {  //email 불일치거나 user객체의 비번이랑 DB해싱비번이 불일치면
         return res.status(400).json({ message: '인증 정보가 유효하지 않습니다.' });
     }
 
-    const checkpassword = await bcrypt.compare(password, user.password);
+    const token = generateAccessToken(user.id);
 
-    if (!checkpassword) {
-        return res.status(400).json({ message: '인증 정보가 유효하지 않습니다.' });
-    }
-
-    return res.status(200).json({ message: '로그인에 성공했습니다.' });
+    return res.status(200).json({
+        message: '로그인에 성공했습니다.',
+        token,       //AccessToken을 반환합니다.
+    });
 });
 
 
