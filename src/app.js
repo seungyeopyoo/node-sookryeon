4f5b0d8963c1dd7875ed18fdaa846439a1fdd92f
yooseungyeop <yooseungyub>
2024-05-27 23:49:27 +0900

    ✨Update 내정보조회API구현+미들웨어수정

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { errorHandler } from './middlewares/error-handler.middleware.js';
-import UserRouter from './routers/auth.router.js';
+import authRouter from './routers/auth.router.js';
+import UserRouter from './routers/users.router.js';
 import ResumeRouter from './routers/resumes.router.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -12,7 +13,7 @@ const port = 3306;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [UserRouter, ResumeRouter]);
+app.use('/api', [UserRouter, ResumeRouter, authRouter]);
 app.use(errorHandler);
 
 app.listen(port, () => {
--- src/middlewares/require-access-token.middleware.js
+++ src/middlewares/require-access-token.middleware.js
@@ -1,20 +1,37 @@
 import jwt from 'jsonwebtoken';
+import { prisma } from '../utils/prisma.util.js';
 
-export const requireAccessToken = (req, res, next) => {
-    // const token = req.headers['authorization']?.split(' ')[1]; 
-    const { accessToken } = req.cookies
-    const decodedToken = accessToken.split(' ')[1];
+export const requireAccessToken = async (req, res, next) => {
+    try {  // const token = req.headers['authorization']?.split(' ')[1]; 
+        const { accessToken } = req.cookies
+        console.log(accessToken);
+        const encodedToken = accessToken.split(' ')[1];
 
-    if (!accessToken) {
-        return res.status(401).json({ message: '토큰이 없습니다.' });
-    }
-
-    jwt.verify(decodedToken, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: '토큰이 유효하지 않습니다.' });
+        if (!accessToken) {
+            return res.status(401).json({ message: '토큰이 없습니다.' });
         }
 
-        req.userId = decoded.id;
+        const decoded = jwt.verify(encodedToken, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: '토큰이 유효하지 않습니다.' });
+            }
+            return decoded
+        });
+
+        const userId = decoded.id;
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId }
+        })
+
+        // →조회 된 사용자 정보를 req.user에 담고, 다음 동작을 진행합니다.
+
+        req.user = user;
+
         next();
-    });
+
+    } catch (err) {
+        next(err);
+    }
 };
+
